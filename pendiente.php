61fed26cabaeec69468a7c86b39724b7122224d3
Diego <difredespelletti@gmail.com>
2021-06-19 01:09:56 -0300

    Cambios Varios
    Cambios

---
+++ pendiente.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pendiente</title>
+</head>
+
+<body>
+    TU COMPRA ESTA PENDIENTE, DIRIGITE A LA SUCURSAL DE "MEDIO DE PAGO EN EFECTIVO" (acá iría información pertinente a el estado pendiente)
+
+</body>
+
+</html>
