b71c46f02cbbbd83e57389b80729f9a2241c989a
Diego <difredespelletti@gmail.com>
2021-06-19 16:48:32 -0300

    Repositorio New
    s

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser" lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    
+
     <meta name="viewport" content="width=1024">
     <title>Tienda e-commerce</title>
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="format-detection" content="telephone=no">
-
+    <script src="https://www.mercadopago.com/v2/security.js" view="home"></script>
     <script
     src="https://code.jquery.com/jquery-3.4.1.min.js"
     integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
     crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="./assets/category-landing.css" media="screen, print">
+    <link rel="stylesheet"  href="{!!asset('assets/category-landing.css')!!}" media="screen, print">
 
-    <link rel="stylesheet" href="./assets/category.css" media="screen, print">
+    <link rel="stylesheet" href="{!!asset('assets/category.css')!!}" media="screen, print">
 
-    <link rel="stylesheet" href="./assets/merch-tools.css" media="screen, print">
+    <link rel="stylesheet" href="{!!asset('assets/merch-tools.css')!!}" media="screen, print">
 
-    <link rel="stylesheet" href="./assets/fonts" media="">
+    <link rel="stylesheet" href="{!!asset('assets/fonts')!!}" media="">
     <style>
         .as-filter-button-text {
             font-size: 26px;
@@ -44,7 +44,7 @@
 <body class="as-theme-light-heroimage">
 
     <div class="stack">
-        
+
         <div class="as-search-wrapper" role="main">
             <div class="as-navtuck-wrapper">
                 <div class="as-l-fullwidth  as-navtuck" data-events="event52">
@@ -205,9 +205,9 @@
                                         <li class="as-accordion-item">
                                             <div id="facet2" class="as-search-facet iPod Compatibility ">
                                                 <div class="as-accordion-header as-search-accordion-header" data-ase-materializer="as-accordion-id2" data-ase-click="toggle">
-                                                   
+
                                                 </div>
-                                                
+
                                             </div>
                                         </li>
 
@@ -237,7 +237,7 @@
                                                         <div class=""></div>
                                                         <img src="./assets/samsung-galaxy-s9-xxl.jpg" class="ir ir item-image as-producttile-image" alt="" data-desc="Samsung galaxy" style="content:-webkit-image-set(url(https://md6.pricebaba.com/images/product/mobile/46638/samsung-galaxy-s9-xxl.jpg">
                                                     </div>
-                                                    
+
                                                 </div>
                                             </div>
                                         </div>
@@ -257,11 +257,12 @@
                                                 $15.000
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
-                                            <input type="hidden" name="img" value="./assets/samsung-galaxy-s9-xxl.jpg">
+                                        <form action="{{ route('product.show')}}" method="post">
+                                            <input type="hidden" name="img" value="samsung-galaxy-s9-xxl.jpg">
                                             <input type="hidden" name="title" value="Samsung Galaxy S9">
                                             <input type="hidden" name="price" value="15000">
                                             <input type="hidden" name="unit" value="1">
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                             <button type="submit" class="mercadopago-button" formmethod="post">Comprar</button>
                                         </form>
                                     </div>
@@ -280,7 +281,7 @@
                                                         <div class=""></div>
                                                         <img src="./assets/l6g6.jpg" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" data-desc="LG G6" style="">
                                                     </div>
-                                                    
+
                                                 </div>
                                             </div>
 
@@ -312,11 +313,12 @@
                                                 $10.000
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
-                                            <input type="hidden" name="img" value="./assets/l6g6.jpg">
+                                        <form action="{{ route('product.show')}}" method="get">
+                                            <input type="hidden" name="img" value="l6g6.jpg">
                                             <input type="hidden" name="title" value="LG G6">
                                             <input type="hidden" name="price" value="10000">
                                             <input type="hidden" name="unit" value="1">
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                             <button type="submit" class="mercadopago-button" formmethod="post">Comprar</button>
                                         </form>
                                     </div>
@@ -347,7 +349,7 @@
                                                 </div>
                                             </div>
 
-                                           
+
 
                                         </div>
 
@@ -370,11 +372,12 @@
                                                 </span>
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
-                                            <input type="hidden" name="img" value="./assets/u_10168742.jpg">
+                                        <form action="{{ route('product.show')}}" method="get">
+                                            <input type="hidden" name="img" value="u_10168742.jpg">
                                             <input type="hidden" name="title" value="iPhone 8">
                                             <input type="hidden" name="price" value="16000">
                                             <input type="hidden" name="unit" value="1">
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                             <button type="submit" class="mercadopago-button" formmethod="post">Comprar</button>
                                         </form>
                                     </div>
@@ -386,7 +389,7 @@
                                             <img src="./assets/wireless-headphones" class="ir ir item-image as-producttile-image  " alt="" width="445" height="445">
                                         </div>
                                         <div class="images mini-gallery gal3 ">
-                                           
+
 
                                             <div class="as-isdesktop with-paddlenav with-paddlenav-onhover">
                                                 <div class="clearfix image-list xs-no-js as-util-relatedlink relatedlink" data-relatedlink="4|BeatsX Earphones - Matte Silver|MR3J2">
@@ -394,7 +397,7 @@
                                                         <div class=""></div>
                                                         <img src="./assets/motorola-moto-g5-plus-1.jpg" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" data-desc="BeatsX Earphones - Matte Silver - Image 1 " style="content:-webkit-image-set(url(https://cdn2.gsmarena.com/vv/pics/motorola/motorola-moto-g5-plus-1.jpg) 1x);">
                                                     </div>
-                                                    
+
                                                 </div>
 
                                             </div>
@@ -427,11 +430,12 @@
                                                 </span>
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
-                                            <input type="hidden" name="img" value="./assets/motorola-moto-g5-plus-1.jpg">
+                                        <form action="{{ route('product.show')}}" method="get">
+                                            <input type="hidden" name="img" value="motorola-moto-g5-plus-1.jpg">
                                             <input type="hidden" name="title" value="Motorola G5">
                                             <input type="hidden" name="price" value="9000">
                                             <input type="hidden" name="unit" value="1">
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                             <button type="submit" class="mercadopago-button" formmethod="post">Comprar</button>
                                         </form>
                                     </div>
@@ -455,10 +459,10 @@
                                                         <div class=""></div>
                                                         <img src="./assets/motorola-moto-g4-3.jpg" class="ir ir item-image as-producttile-image" style="content:-webkit-image-set(url(https://cdn2.gsmarena.com/vv/pics/motorola/motorola-moto-g4-3.jpg) 1x);">
                                                     </div>
-                                                    
+
                                                 </div>
 
-                                               
+
                                             </div>
 
                                         </div>
@@ -477,11 +481,12 @@
                                                 $8.000
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
-                                            <input type="hidden" name="img" value="./assets/motorola-moto-g4-3.jpg">
+                                        <form action="{{ route('product.show')}}" method="get">
+                                            <input type="hidden" name="img" value="motorola-moto-g4-3.jpg">
                                             <input type="hidden" name="title" value="Moto G4">
                                             <input type="hidden" name="price" value="8000">
                                             <input type="hidden" name="unit" value="1">
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                             <button type="submit" class="mercadopago-button" formmethod="post">Comprar</button>
                                         </form>
                                     </div>
@@ -493,7 +498,7 @@
                                             <img src="./assets/wireless-headphones" class="ir ir item-image as-producttile-image  " alt="" width="445" height="445">
                                         </div>
                                         <div class="images mini-gallery gal5 ">
-                                       
+
 
                                             <div class="as-isdesktop with-paddlenav with-paddlenav-onhover">
                                                 <div class="clearfix image-list xs-no-js as-util-relatedlink relatedlink" data-relatedlink="6|Powerbeats3 Wireless Earphones - Neighborhood Collection - Brick Red|MPXP2">
@@ -501,13 +506,13 @@
                                                         <div class=""></div>
                                                         <img src="./assets/003.jpg" class="ir ir item-image as-producttile-image" alt="" width="445" height="445" style="content:-webkit-image-set(url(https://news.mynavi.jp/article/20180416-xperia-next/images/003.jpg) 1x);">
                                                     </div>
-                                                    
+
                                                 </div>
 
-                                                
+
                                             </div>
 
-                                            
+
 
                                         </div>
 
@@ -528,11 +533,12 @@
                                                 </span>
                                             </div>
                                         </div>
-                                        <form action="/detail.php" method="get">
-                                            <input type="hidden" name="img" value="./assets/003.jpg">
+                                        <form action="{{ route('product.show')}}" method="get">
+                                            <input type="hidden" name="img" value="003.jpg">
                                             <input type="hidden" name="title" value="Sony Xperia XZ2">
                                             <input type="hidden" name="price" value="10000">
                                             <input type="hidden" name="unit" value="1">
+                                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                             <button type="submit" class="mercadopago-button" formmethod="post">Comprar</button>
                                         </form>
                                     </div>
