06c668ec83fbbd60ec14a460a738041ac5b84955
Diego <difredespelletti@gmail.com>
2021-06-19 16:52:18 -0300

    New Repository
    test

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+require __DIR__  . '/../../../vendor/autoload.php';
+
+use MercadoPago\Item;
+use MercadoPago\MerchantOrder;
+use MercadoPago\Payer;
+use MercadoPago\Payment;
+use MercadoPago\Preference;
+use MercadoPago\SDK;
+
+class NotificationController extends Controller
+{
+
+
+	public function getNotification(Request $request){
+
+
+   	 SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+
+    switch($_GET["topic"]) {
+
+        case "payment":
+            $payment = Payment::find_by_id($_GET["id"]);
+
+            $response=json_encode($payment, true);
+            var_dump($payment);
+
+            break;
+        case "merchant_order":
+            $plan = MerchantOrder::find_by_id($_GET["id"]);
+            $response=json_encode($plan, true);
+            var_dump($plan);
+            break;
+        case "subscription":
+            $plan = Subscription::find_by_id($_POST["id"]);
+            $response=json_decode($plan, true);
+            console_log($response);
+            break;
+        case "invoice":
+            $plan = Invoice::find_by_id($_POST["id"]);
+            $response=json_decode($plan, true);
+            var_dump($response);
+            break;
+    }
+
+    return response('OK', 200);
+
+   	}
+
+
+   	public function postNotification(Request $request){
+
+
+   	 SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+
+    switch($_POST["topic"]) {
+
+        case "payment":
+            $payment = Payment::find_by_id($_POST["id"]);
+            $response=json_encode($payment, true);
+            var_dump($response);
+
+            break;
+        case "merchant_order":
+            $plan = MerchantOrder::find_by_id($_POST["id"]);
+            $response=json_encode($plan, true);
+            var_dump($response);
+            break;
+        case "subscription":
+            $plan = Subscription::find_by_id($_POST["id"]);
+            $response=json_decode($plan, true);
+            console_log($response);
+            break;
+        case "invoice":
+            $plan = Invoice::find_by_id($_POST["id"]);
+            $response=json_decode($plan, true);
+            var_dump($response);
+            break;
+    }
+
+    return response('OK', 200);
+
+   	}
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/','IndexController@index')->name('index.index');
+
+Route::post('detail', 'ProductController@show')->name('product.show');
+
+Route::post('/checkout/{title},{price},{unit},{img}','CheckOutController@index')->name('checkout');
+Route::get('/backurl/success','BackUrlController@success')->name('backurl.success');
+
+Route::get('/backurl/failure','BackUrlController@failure')->name('backurl.failure');
+
+Route::get('/backurl/pending','BackUrlController@pending')->name('backurl.pending');
+
+Route::post('notification','NotificationController@postNotification')->name('notification');
+
+Route::get('notification','NotificationController@getNotification')->name('notification');
+
+
+//Route::get('notification','BackUrlController@notification')->name('notification');
